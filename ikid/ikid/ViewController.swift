1aab9d8f8ddb29902f61c030590f32ff0f1457a1
israelrmartinez <israel.r.mar@gmail.com>
2022-05-02 19:56:09 -0700

    first commit

---
+++ ikid/ikid/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ikid
+//
+//  Created by stlp on 5/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
